2c210906a3d717b4096efc653326c05e7b4bc170
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-06-24 13:37:30 +0300

    Updates

---
+++ assets/ProductsList-UuKfms2v.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-oYNwcgl-.js";import{P as o}from"./ProductCard-Cyi4-oka.js";const m=({products:t,displayType:i})=>s.jsx("div",{className:"productsList",children:t.map(r=>s.jsx("div",{className:"productItem",children:s.jsx(o,{product:r,displayType:i})},r.id))});export{m as P};
